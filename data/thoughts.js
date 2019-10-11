b8cbaa890c87ebe56620d494fa4ff8d54f45461f
UncleJerry <jared.myhrberg@gmail.com>
2019-10-11 12:45:23 -0700

    time filter funciton working

---
+++ data/thoughts.js
@@ -0,0 +1,46 @@
+function hours(i) {
+  return i * 100 * 60 * 60;
+}
+
+export const thoughts = [
+  {
+    thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
+    time: Date.now() - hours(0)
+  },
+  {
+    thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
+    time: Date.now() - hours(0)
+  },
+  {
+    thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
+    time: Date.now() - hours(2)
+  },
+  {
+    thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
+    time: Date.now() - hours(2)
+  },
+  {
+    thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
+    time: Date.now() - hours(5)
+  },
+  {
+    thought: 'heres my thought about the day',
+    time: Date.now() - hours(5)
+  },
+  {
+    thought: 'heres my thought about the day',
+    time: Date.now() - hours(11)
+  },
+  {
+    thought: 'heres my thought about the day',
+    time: Date.now() - hours(11)
+  },
+  {
+    thought: 'heres my thought about the day',
+    time: Date.now() - hours(23)
+  },
+  {
+    thought: 'heres my thought about the day',
+    time: Date.now() - hours(30)
+  }
+];
--- src/services/thought-service.js
+++ src/services/thought-service.js
@@ -1,46 +1,7 @@
+import { thoughts } from '../../data/thoughts';
+
 export function fetchThoughts() {
 
-  return [
-    {
-      thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day. heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day',
-      time: new Date()
-    },
-    {
-      thought: 'heres my thought about the day',
-      time: new Date()
-    }
-  ];
+  return thoughts;
 }
 
